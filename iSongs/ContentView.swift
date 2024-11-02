68ba21f99ba838389508baf720f3712069c0a763
JoelHJames21 <joel.hernandezjames@mmgct.com>
2024-11-02 17:44:59 -0500

    Initial Commit

---
+++ iSongs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSongs
+//
+//  Created by Joel Hernandez on 11/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSongs/iSongsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSongsApp.swift
+//  iSongs
+//
+//  Created by Joel Hernandez on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iSongsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
